ca8a77a1020660ed2fec8b955e3788997563db67
zehnpaard <zehnpaard@googlemail.com>
2018-11-10 11:32:52 +0900

    Implement terms and termShift

---
+++ ch07/lambda.ml
@@ -0,0 +1,14 @@
+type term =
+  | TmVar of int
+  | TmAbs of term
+  | TmApp of term * term
+;;
+
+let termShift d t =
+    let rec walk c = function
+      | TmVar x -> if x >= c then TmVar (x+d) else TmVar x
+      | TmAbs t1 -> TmAbs (walk (c+1) t1)
+      | TmApp (t1, t2) -> TmApp (walk c t1, walk c t2)
+    in
+    walk 0 t
+;;
