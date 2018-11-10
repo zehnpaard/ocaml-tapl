92db85a3537d31cf1ccde9572d5f138f48942c9f
zehnpaard <zehnpaard@googlemail.com>
2018-11-10 20:08:10 +0900

    Add typeOf

--- ch10/simple.ml
+++ ch10/simple.ml
@@ -11,3 +11,29 @@ type term =
   | TmAbs of ty * term
   | TmApp of term * term
 ;;
+
+exception TypeError;;
+let rec typeOf ctx = function
+  | TmTrue -> TyBool
+  | TmFalse -> TyBool
+  | TmIf (t1, t2, t3) ->
+          if TyBool != typeOf ctx t1
+          then raise TypeError
+          else
+              let ty2 = typeOf ctx t2 in
+              let ty3 = typeOf ctx t3 in
+              if ty2 != ty3
+              then raise TypeError
+              else ty2
+  | TmVar n -> getTypeFromContext ctx n
+  | TmAbs (tya, tb) ->
+          let ctx' = addbinding ctx (VarBind tya) in
+          let tyr = typeOf ctx' tb in
+          TyArrow (tya, tyr)
+  | TmApp (t1, t2) ->
+          let ty1 = typeOf ctx t1 in
+          let ty2 = typeOf ctx t2 in
+          match ty1 with
+            | TyArrow (tya, tyr) when ty2 = tya -> tyr
+            | _ -> raise TypeError
+;;
