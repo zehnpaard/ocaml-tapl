9bf3eee96642369b23fdeea08d1fecab5d6f808f
zehnpaard <zehnpaard@googlemail.com>
2018-11-10 12:13:36 +0900

    Implement termSubst and termSubstTop

--- ch07/lambda.ml
+++ ch07/lambda.ml
@@ -12,3 +12,16 @@ let termShift d t =
     in
     walk 0 t
 ;;
+
+let termSubst j s t =
+    let rec walk c = function
+      | TmVar x -> if x = j+c then termShift c s else TmVar x
+      | TmAbs t1 -> TmAbs (walk (c+1) t1)
+      | TmApp (t1, t2) -> TmApp (walk c t1, walk c t2)
+    in
+    walk 0 t
+;;
+
+let termSubstTop s t =
+    termShift (-1) (termSubst 0 (termShift 1 s) t)
+;;
