fc7c2ff1630fade9002604f937da3b9ded3ab06b
zehnpaard <zehnpaard@googlemail.com>
2018-11-11 22:08:04 +0900

    Implement tymap

--- ch25/systemf.ml
+++ ch25/systemf.ml
@@ -9,3 +9,13 @@ type binding =
   | VarBind of ty
   | TyVarBind
 ;;
+
+let tymap onvar c tyt =
+  let rec walk c tyt = match tyt with
+    | TyArrow (tyt1, tyt2) -> TyArrow (walk c tyt1, walk c tyt2)
+    | TyVar n -> onvar c n
+    | TyAll tyt2 -> TyAll (walk (c+1) tyt2)
+    | TySome tyt2 -> TySome (walk (c+1) tyt2)
+  in
+  walk c tyt
+;;
