295f8e8000ea18e9e5d4dcbbb433e379f308225d
zehnpaard <zehnpaard@googlemail.com>
2018-11-11 23:09:16 +0900

    Implement typeShiftAbove using tymap

--- ch25/systemf.ml
+++ ch25/systemf.ml
@@ -13,9 +13,18 @@ type binding =
 let tymap onvar c tyt =
   let rec walk c tyt = match tyt with
     | TyArrow (tyt1, tyt2) -> TyArrow (walk c tyt1, walk c tyt2)
-    | TyVar n -> onvar c n
+    | TyVar x -> onvar c x
     | TyAll tyt2 -> TyAll (walk (c+1) tyt2)
     | TySome tyt2 -> TySome (walk (c+1) tyt2)
   in
   walk c tyt
+;;
+
+let typeShiftAbove d c tyt =
+    let f c x =
+        if x >= c
+        then TyVar (x+d)
+        else TyVar x
+    in
+    tymap f c tyt
 ;;
